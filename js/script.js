c89ab095be0a968fe7c8b3b908641cbb6dda0825
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-03 09:31:38 -0500

    [add] thumbs with v-for

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
-// Esercizio di oggi: Vue Slider
-// nome repo: vue-slider
-// Descrizione:
 // Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
 // - al click su una thumb, visualizzare in grande l’immagine corrispondente
 // - aggiungere la classe active alla thumb attiva
 // Bonus:
 // 1- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 // 2- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
-// Consigli del giorno:
-// - regola d’oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo recuperare html e css dell’esercizio svolto qualche giorno fa: è già tutto pronto!
-// - il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l’esercizio si riduce a poche righe ;)
-// Buon lavoro e buon divertimento!
+
+const { createApp } = Vue;
+
+createApp({
+  data(){
+    return {
+      slider: [
+        {
+          title: "Obelisco",
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, sunt.",
+          photo: "/img/01.jpg"
+        },
+        {
+          title: "La pelota",
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, sunt.",
+          photo: "/img/02.jpg"
+        },
+        {
+          title: "Tango",
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, sunt.",
+          photo: "/img/03.jpg"
+        },
+        {
+          title: "La Murga",
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, sunt.",
+          photo: "/img/04.jpg"
+        },
+        {
+          title: "Boxeo",
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, sunt.",
+          photo: "/img/05.jpg"
+        }
+      ],
+      activeImage: 0
+    }
+  }
+}).mount('#app')
